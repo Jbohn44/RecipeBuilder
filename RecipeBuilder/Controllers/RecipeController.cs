64a8019a143f86268fa7580606b31ef6757e639f
ejbohnert <ejbohnert@outlook.com>
2018-07-04 20:29:18 -0500

    initial commit

---
+++ RecipeBuilder/Controllers/RecipeController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using RecipeBuilder.Models;
+
+namespace RecipeBuilder.Controllers
+{
+    public class RecipeController : Controller
+    {
+        public IActionResult Index()
+        {
+            IList<Recipe> recipes = new List<Recipe>();
+            return View(recipes);
+        }
+
+        public IActionResult Add()
+        {
+            return View();
+        }
+    }
+}
---
+++ RecipeBuilder/Models/Recipe.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RecipeBuilder.Models
+{
+    public class Recipe
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+
+        public IList<Ingredient> ingredients { get; set; }
+        
+        public string Instructions { get; set; }
+    }
+}
