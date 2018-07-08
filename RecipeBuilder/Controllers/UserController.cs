d12133d7e32a26f6630ce7256371a3977bbf19d6
ejbohnert <ejbohnert@outlook.com>
2018-07-07 20:14:15 -0500

    Added Some more user sign up functionality

--- RecipeBuilder/Controllers/UserController.cs
+++ RecipeBuilder/Controllers/UserController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using RecipeBuilder.ViewModels;
 
 namespace RecipeBuilder.Controllers
 {
@@ -11,6 +12,12 @@ namespace RecipeBuilder.Controllers
         public IActionResult Index()
         {
             return View();
+        }
+
+        public IActionResult Add()
+        {
+            AddUserViewModel addUserViewModel = new AddUserViewModel();
+            return View(addUserViewModel);
         }
     }
 }
--- RecipeBuilder/ViewModels/AddUserViewModel.cs
+++ RecipeBuilder/ViewModels/AddUserViewModel.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,5 +8,28 @@ namespace RecipeBuilder.ViewModels
 {
     public class AddUserViewModel
     {
+        [Display(Name = "Email Address")]
+        [Required(ErrorMessage = "Email Address Required")]
+        [DataType(DataType.EmailAddress)]
+        public string Email { get; set; }
+
+        [Display(Name = "Username")]
+        [Required(ErrorMessage = "Username Required")]
+        [Range(8, 20)]
+        public string Username { get; set; }
+
+        [Display(Name = "Password")]
+        [Required(ErrorMessage = "Password Required")]
+        [DataType(DataType.Password)]
+        [MinLength(8)]
+        [MaxLength(25)]
+        public string Password { get; set; }
+
+        [Display(Name ="Confirm Password")]
+        [Required(ErrorMessage ="Please Confirm Password")]
+        [DataType(DataType.Password)]
+        [Compare("Password")]
+        public string ConfirmPassword { get; set; }
+
     }
 }
